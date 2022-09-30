5a78098b5b26e27aaa3258063ec3fdc5ad8259a2
epodbrdska <eva.podbrdska@showmax.com>
2022-09-30 16:02:13 +0200

    More tests and data management

--- requests/api.rb
+++ requests/api.rb
@@ -5,9 +5,7 @@ require_relative '../spec/spec_helper'
 module Room
 
   def self.conn
-    Faraday.new(url: "https://automationintesting.online") do |faraday|
-      faraday.response :logger, nil, { headers: true, bodies: true }
-    end
+    Faraday.new(url: "https://automationintesting.online")
   end
 
   def self.login
@@ -17,11 +15,17 @@ module Room
     end
   end
 
-  def self.create_room(cookie, payload)
+  def self.create_room(cookie = nil, payload)
     conn.post "room/" do |req|
       req.headers['Content-Type'] = 'application/json'
       req.headers['Cookie'] = cookie
       req.body = payload
+    end
+  end
+
+  def self.delete_room(cookie, roomid)
+    conn.delete("room/#{roomid}") do |req|
+      req.headers['Cookie'] = cookie
     end
   end
 
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,29 +2,49 @@ require './requests/api.rb'
 
 describe 'Room API' do
 
-  before(:each) do
-    login = Room.login
-    expect(login.status).to be(200)
-    @cookie = login.headers['set-cookie']
+  context 'with a valid token' do
+    before(:each) do
+      login = Room.login
+      expect(login.status).to be(200)
+      @cookie = login.headers['set-cookie']
+    end
+
+    after(:each) do
+      Room.delete_room(@cookie, @roomid)
+    end
+
+    it 'successfully creates a room without features' do
+      create_room = Room.create_room(@cookie, Payloads.room_no_features)
+      expect(create_room.status).to be(201)
+      expect(create_room.body).to match_schema('create_room')
+      json = JSON.parse(create_room.body)
+      @roomid = json['roomid']
+    end
+
+    it 'successfully creates a room with features' do
+      create_room = Room.create_room(@cookie, Payloads.room_with_features)
+      expect(create_room.status).to be(201)
+      expect(create_room.body).to match_schema('create_room')
+      json = JSON.parse(create_room.body)
+      @roomid = json['roomid']
+    end
+
+    it "doesn't allow to create same room name multiple times" do
+      create_room = Room.create_room(@cookie, Payloads.room_with_features)
+      expect(create_room.status).to be(201)
+      json = JSON.parse(create_room.body)
+      @roomid = json['roomid']
+      create_second_room = Room.create_room(@cookie, Payloads.room_with_features)
+      expect(create_second_room.status).to be(409)
+    end
+
   end
 
-  it 'successfully creates a room without features' do
-    create_room = Room.create_room(@cookie, Payloads.room_no_features)
-    expect(create_room.status).to be(201)
-    expect(create_room.body).to match_schema('create_room')
-  end
-
-  it 'successfully creates a room with features' do
-    create_room = Room.create_room(@cookie, Payloads.room_with_features)
-    expect(create_room.status).to be(201)
-    expect(create_room.body).to match_schema('create_room')
-  end
-
-  it "doesn't allow to create same room name multiple times" do
-    create_room = Room.create_room(@cookie, Payloads.room_with_features)
-    expect(create_room.status).to be(201)
-    create_second_room = Room.create_room(@cookie, Payloads.room_with_features)
-    expect(create_second_room.status).to be(409)
+  context 'without a valid token' do
+    it 'is unable to create a room' do
+      create_room = Room.create_room(Payloads.room_no_features)
+      expect(create_room.status).to be(403)
+    end
   end
 
 end
